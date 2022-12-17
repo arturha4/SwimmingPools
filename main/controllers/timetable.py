6515a8d434a84c1b24e1471ec904b3f1923fddde
pivasikpivtsakov <elda250@live.com>
2022-12-18 00:29:15 +0600

    timetable get/post, intersections, correctness

--- main/controllers/timetable.py
+++ main/controllers/timetable.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,8 +13,23 @@ class Timetable(APIView):
     permission_classes = []
 
     def get(self, request):
-        return TimetableSlot.objects.get()
+        return Response(TimetableSlot.objects.all(), status.HTTP_200_OK)
 
     def post(self, request):
+        # перепишу на сериализаторах потом
+        # создает слот и возвращает результат его правильности
+        # все равно записывает, даже если невалидный, потом доделаю
         data = request.data
-        return Response(TimetableSlot.objects.create(data))
+        timetable_slot = TimetableSlot.objects.create(**data)
+        start = datetime.fromisoformat('2022-12-17T19:06:07')
+        end = datetime.fromisoformat('2022-12-17T21:06:07')
+
+        intersects = timetable_slot.intersects_with_slot({'start': start, 'end': end})
+        correct = timetable_slot.is_correct()
+        return Response(
+            {
+                'intersects': intersects,
+                'correct': correct,
+            },
+            status.HTTP_200_OK
+        )
