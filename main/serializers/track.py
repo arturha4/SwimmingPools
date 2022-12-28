e50b3139400b9eaa275249c51d5b6744f2699903
pivasikpivtsakov <elda250@live.com>
2022-12-28 19:41:19 +0600

    rm shit

--- main/serializers/track.py
+++ main/serializers/track.py
@@ -6,8 +6,6 @@ from main.models.track import Track
 class TrackAvailableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
-        fields = ['id', 'number', 'available_places']
+        fields = ['id', 'number']
 
-    def get_available_places(self):
-        pass
 # здесь можно приделать колво доступных для записи мест...
