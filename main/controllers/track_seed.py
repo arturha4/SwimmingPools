65514753d445c481148350355576cc06ec4c4ec2
pivasikpivtsakov <elda250@live.com>
2023-01-24 01:07:27 +0600

    tracks rm auth

--- main/controllers/track_seed.py
+++ main/controllers/track_seed.py
@@ -6,6 +6,9 @@ from main.models.track import Track
 
 
 class TrackSeed(APIView):
+    authentication_classes = []
+    permission_classes = []
+
     def post(self, request):
         """cоздать дорожки, вызывает разраб с постмана"""
         for i in range(1, 7):
